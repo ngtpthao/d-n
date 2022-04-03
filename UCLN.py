96955a15b811a7422d13c8088cd6bd9d429891a6
ngtpthao <95376678+ngtpthao@users.noreply.github.com>
2022-04-03 21:12:09 +0700

    Add files via upload

---
+++ UCLN.py
@@ -0,0 +1,12 @@
+def USCLN(a,b):
+    us=1
+    for i in range(2,a):
+        if i%a==0 and b%i==0:
+            us=i
+            return us
+def test_func():
+    """"
+    :return: test
+    """"""
+    pass
+print ('hello')
